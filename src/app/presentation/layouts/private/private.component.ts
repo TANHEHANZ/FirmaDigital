e9fb22d3f925b7cb5ef0dd3de5b4004a3cfd0140
TANHEHANZ <hantach10@gmail.com>
2025-02-04 23:25:56 -0400

    feat:create layaut custom properties

--- src/app/presentation/layouts/private/private.component.ts
+++ src/app/presentation/layouts/private/private.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from '../../shared/topbar/topbar.component';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-private',
-  imports: [RouterOutlet],
-  template: `<main>
-    <router-outlet />
-  </main> `,
+  imports: [RouterOutlet, TopbarComponent, SidebarComponent],
+  template: `
+    <main
+      class="h-screen w-screen grid  grid-cols-[230px_1fr] grid-rows-[auto_1fr] "
+    >
+      <app-sidebar class=" h-full row-span-2 shadow-2xl" />
+      <section class=" row-span-2 flex flex-col ">
+        <app-topbar class="h-auto w-full z-50  p-1 shadow-md " />
+        <router-outlet />
+      </section>
+    </main>
+  `,
 })
 export class PrivateComponent {}
