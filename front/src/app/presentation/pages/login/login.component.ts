6e572495d532f88dac4898d0ed2c91c8d6435051
TANHEHANZ <hantach10@gmail.com>
2025-02-13 15:25:54 -0400

    feat:redirect to path login

--- front/src/app/presentation/pages/login/login.component.ts
+++ front/src/app/presentation/pages/login/login.component.ts
@@ -1,13 +1,27 @@
 import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ButtonPrimaryComponent } from '../../shared/ui/button/primary.component';
 import { ICONS } from '../../shared/ui/icons';
 import { PATH_ROUTES } from '../../../application/models/route.enum';
 import { ButtonSecundaryComponent } from '../../shared/ui/button/secundary.component';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonPrimaryComponent, ButtonSecundaryComponent],
+  imports: [
+    ButtonPrimaryComponent,
+    ButtonSecundaryComponent,
+    ReactiveFormsModule,
+    NgClass,
+    NgIf,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -16,5 +30,32 @@ export class LoginComponent {
   router = inject(Router);
   logout() {
     this.router.navigate([PATH_ROUTES.DASHBOARD_FIRMAR]);
+  }
+
+  form = signal<FormGroup>(
+    new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+        Validators.maxLength(150),
+      ]),
+    })
+  );
+  validar() {
+    console.log(this.form().value);
+  }
+  isFieldRequired(fieldName: string): boolean {
+    const control = this.form().get(fieldName);
+    if (control && control.validator) {
+      const validator = control.validator({} as AbstractControl);
+      return validator && validator['required'];
+    }
+    return false;
   }
 }
--- front/src/app/presentation/shared/ui/input.component.ts
+++ front/src/app/presentation/shared/ui/input.component.ts
@@ -1,28 +1,41 @@
 import { NgIf } from '@angular/common';
-import { Component, EventEmitter, input, Output, output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'Basic-input',
-  imports: [NgIf],
-
+  selector: 'basic-input',
+  standalone: true,
+  imports: [NgIf, ReactiveFormsModule],
   template: `
     <div class="flex flex-col gap-1 w-full my-2 items-start">
-      <label for="" class="block"> {{ label() }} </label>
-      <input type="text" class="border flex-1 rounded-md p-1 px-2 w-full" />
-      <label
-        *ngIf="icon()"
-        class=" flex  justify-center items-center h-full bg-white text-black px-4 "
-      >
-        <i [class]="icon()"></i>
+      <label for="" class="block">
+        {{ label() }}
+        <span *ngIf="isRequired()" class="text-red-500">*</span>
       </label>
+      <input
+        [type]="type()"
+        [placeholder]="placeholder()"
+        [formControl]="inputControl"
+        (input)="onInput($event)"
+        class="border flex-1 rounded-md p-1 px-2 w-full outline-none border-gray-300"
+      />
     </div>
   `,
 })
 export class BasicInputComponent {
-  label = input<string>();
-  icon = input<string>();
-  clicked = output<void>();
-  onClick() {
-    this.clicked.emit();
+  label = input<string>('');
+  type = input<string>('');
+  placeholder = input<string>('');
+  isRequired = input<boolean>(false);
+  valueChange = output<string>();
+
+  inputControl = new FormControl(
+    '',
+    this.isRequired() ? [Validators.required] : []
+  );
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.valueChange.emit(value);
   }
 }
