8c379f5b565937843893b3542c29e59443dd0e4a
TANHEHANZ <hantach10@gmail.com>
2025-02-14 07:08:01 -0400

    fix:manage cors add dependeci

--- back/index.ts
+++ back/index.ts
@@ -1,9 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./src/routes";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT;
 app.use("/v1/api", router);
