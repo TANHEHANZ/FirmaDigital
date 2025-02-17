32d59905aaae9854bd352dcba70fb566b95812d6
TANHEHANZ <hantach10@gmail.com>
2025-02-17 07:28:28 -0400

    feat:manage dto user add validations

--- back/src/infraestrucure/DTO/user.dto.ts
+++ back/src/infraestrucure/DTO/user.dto.ts
@@ -3,26 +3,47 @@ import { z } from "zod";
 export const userSchema = z.object({
   name: z
     .string()
-    .min(3, "El nombre debe tener al menos 3 caracteres")
-    .max(150, "El nombre no puede superar los 150 caracteres"),
+    .min(3, { message: "El nombre debe tener al menos 3 caracteres" })
+    .max(150, { message: "El nombre no puede superar los 150 caracteres" }),
 
-  email: z.string().email("Debe ser un email válido"),
+  email: z.string().email({ message: "Debe ser un email válido" }),
+
   ci: z
     .string()
-    .min(7, "El CI debe tener al menos 7 caracteres")
-    .max(10, "El CI no puede superar los 10 caracteres"),
-  password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
+    .min(7, { message: "El CI debe tener al menos 7 caracteres" })
+    .max(10, { message: "El CI no puede superar los 10 caracteres" })
+    .regex(/^\d+$/, { message: "El CI solo puede contener números" }),
+
+  password: z
+    .string()
+    .min(7, { message: "La contraseña debe tener al menos 7 caracteres" })
+    .regex(/[A-Z]/, {
+      message: "La contraseña debe contener al menos una mayúscula",
+    })
+    .regex(/[a-z]/, {
+      message: "La contraseña debe contener al menos una minúscula",
+    })
+    .regex(/[0-9]/, {
+      message: "La contraseña debe contener al menos un número",
+    })
+    .regex(/[\W_]/, {
+      message: "La contraseña debe contener al menos un carácter especial",
+    }),
+
   tipo_user: z.enum(["Juridica", "natural"], {
     errorMap: () => ({
       message: "El tipo de usuario debe ser 'Juridica' o 'natural'",
     }),
   }),
-  idRol: z.string().min(1, "El rol es obligatorio"),
-  idUnidad: z.string().min(1, "La unidad es obligatoria"),
+
+  idRol: z.string().min(1, { message: "El rol es obligatorio" }),
+  idUnidad: z.string().min(1, { message: "La unidad es obligatoria" }),
+
   refresh_token: z.string().optional(),
-  is_active: z.string().optional().default("FALSE"),
-  is_deleted: z.string().optional().default("FALSE"),
-  isUpdate: z.string().optional().default("FALSE"),
+
+  is_active: z.literal("FALSE").readonly().default("FALSE"),
+  is_deleted: z.literal("FALSE").readonly().default("FALSE"),
+  isUpdate: z.literal("FALSE").readonly().default("FALSE"),
 });
 
 export type UserDTO = z.infer<typeof userSchema>;
