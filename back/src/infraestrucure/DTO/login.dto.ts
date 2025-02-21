38111ddd58bdd12bcd16686e791c0252e97c6620
TANHEHANZ <hantach10@gmail.com>
2025-02-21 14:30:07 +0100

    fix(login):error peyload and fields

--- back/src/infraestrucure/DTO/login.dto.ts
+++ back/src/infraestrucure/DTO/login.dto.ts
@@ -1,29 +1,16 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  email: z
+  ci: z
     .string()
-    .email({ message: "El correo electrónico no es válido" })
-    .min(5, { message: "El campo email debe tener como mínimo 5 caracteres" })
+    .min(7, { message: "El campo ci debe tener como mínimo 5 caracteres" })
     .max(150, {
-      message: "El campo email debe tener como máximo 150 caracteres",
+      message: "El campo ci debe tener como máximo 150 caracteres",
     }),
 
   password: z
     .string()
-    .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
-    .max(100, { message: "La contraseña no debe superar los 100 caracteres" })
-    .regex(/[A-Z]/, {
-      message: "La contraseña debe contener al menos una letra mayúscula",
-    })
-    .regex(/[a-z]/, {
-      message: "La contraseña debe contener al menos una letra minúscula",
-    })
-    .regex(/[0-9]/, {
-      message: "La contraseña debe contener al menos un número",
-    })
-    .regex(/[\W_]/, {
-      message: "La contraseña debe contener al menos un carácter especial",
-    }),
+    .min(7, { message: "La contraseña debe tener al menos 8 caracteres" })
+    .max(100, { message: "La contraseña no debe superar los 100 caracteres" }),
 });
 export type LoginDTO = z.infer<typeof loginSchema>;
