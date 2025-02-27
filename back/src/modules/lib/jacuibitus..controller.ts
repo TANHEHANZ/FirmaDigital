81206392118bb9cb1276dcfeb178080fbb5202e8
TANHEHANZ <hantach10@gmail.com>
2025-02-27 17:32:45 +0100

    feat(Jacuibitus controller):add validation for signed

--- back/src/modules/lib/jacuibitus..controller.ts
+++ back/src/modules/lib/jacuibitus..controller.ts
@@ -45,75 +45,106 @@ export const listToken = async (req: Request, res: Response) => {
 
 export const firmar = async (req: Request, res: Response) => {
   const { slot, alias, pin, nombre, tipo_documento, pdf } = req.body;
+  const idUser = req.user?.userId;
   const id_historial = req.params.id_historial || null;
   try {
-    const response = await fetch(jacubitus + PATH_LIB.UPLOAD_FILE_PDF, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        slot,
-        alias,
-        pin,
-        pdf,
-      }),
-      agent: new https.Agent({
-        rejectUnauthorized: false,
-      }),
-    });
-
-    const data = (await response.json()) as resJacubitus<dataPdf>;
-    if (!data.finalizado) {
-      ManageResponse.notFound(res, "No hay token conectados");
-      return;
-    }
-    // aca me devuelve el pdf firmado en base64
-    const document = await prisma.documento.create({
-      data: {
-        nombre,
-        tipo_documento,
-        documento_blob: data.datos.pdf_firmado,
-        estado: "ACTIVO",
-        id_historial: id_historial ? id_historial : null,
-      },
-    });
-    if (!document) {
-      ManageResponse.notFound(res, "No se pudo guardar el documento");
-      return;
-    }
-    const userId = req.user?.userId;
-    const firmar = await prisma.firmar.create({
-      data: {
-        idDocumento: document.id,
-        idUser: userId!,
-      },
-      omit: {
-        idDocumento: true,
-        idUser: true,
-      },
-      include: {
-        Documento: true,
-        User: {
-          omit: {
-            idRol: true,
-            password: true,
-            refresh_token: true,
+    const validateTokenAsignado = await prisma.asignacionToken.findFirst({
+      where: {
+        id_user_asignado: idUser,
+        NOT: {
+          estado: "DESHABILITADO",
+        },
+        AND: {
+          token: {
+            NOT: {
+              estado_token: "DESHABILITADO",
+            },
           },
         },
       },
     });
-    if (!firmar) {
-      ManageResponse.notFound(
+    if (!validateTokenAsignado) {
+      ManageResponse.notFound(res, "No tienes un token asignado para firmar");
+      return;
+    }
+    try {
+      const response = await fetch(jacubitus + PATH_LIB.UPLOAD_FILE_PDF, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          slot,
+          alias,
+          pin,
+          pdf,
+        }),
+        agent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      });
+
+      const data = (await response.json()) as resJacubitus<dataPdf>;
+      if (!data.finalizado) {
+        ManageResponse.notFound(res, "No hay token conectados");
+        return;
+      }
+      const document = await prisma.documento.create({
+        data: {
+          nombre,
+          tipo_documento,
+          documento_blob: data.datos.pdf_firmado,
+          estado: "ACTIVO",
+          id_historial: id_historial ? id_historial : null,
+        },
+      });
+      if (!document) {
+        ManageResponse.notFound(res, "No se pudo guardar el documento");
+        return;
+      }
+      const userId = req.user?.userId;
+      const firmar = await prisma.firmar.create({
+        data: {
+          idDocumento: document.id,
+          idUser: userId!,
+        },
+        omit: {
+          idDocumento: true,
+          idUser: true,
+        },
+        include: {
+          Documento: true,
+          User: {
+            omit: {
+              idRol: true,
+              password: true,
+              refresh_token: true,
+            },
+          },
+        },
+      });
+      if (!firmar) {
+        ManageResponse.notFound(
+          res,
+          "Error no se pudo gardar todos los datos de la firma"
+        );
+      }
+
+      ManageResponse.success(res, data.mensaje, firmar);
+    } catch (error) {
+      console.error(error);
+      ManageResponse.serverError(
         res,
-        "Error no se pudo gardar todos los datos de la firma"
+        "Error al obtener lista de tokens",
+        error
       );
     }
-
-    ManageResponse.success(res, data.mensaje, firmar);
   } catch (error) {
-    console.error(error);
-    ManageResponse.serverError(res, "Error al obtener lista de tokens", error);
+    ManageResponse.serverError(
+      res,
+      "Error en el servidor contactate con hancito , Te ayudara !!! ",
+      error
+    );
   }
 };
